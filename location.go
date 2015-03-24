5ea4ce6dcda3289eab0a41c297b0118e482c08e8
Tim Shannon <shannon.timothy@gmail.com>
2015-03-24 08:58:49 -0500

    Changed default locations to give highest priority to running directory.  Makes more sense for development, and allows an override.

--- location.go
+++ location.go
@@ -28,21 +28,20 @@ import (
 //StandardFileLocations builds an OS specific list of standard file locations
 // for where a config file should be loaded from.
 // Generally follows this priority list:
-// 1. User locations are used before...
-// 2. System locations which are used before ...
-// 3. The imediate running directory of the application
+// 1. The imediate running directory of the application
+// 2. User locations are used before...
+// 3. System locations which are used before ...
 // The result set will be joined with the passed in filepath.  Passing in
 // a filepath with a leading directory is encouraged to keep your config folders
 // clean.
 //
 // For example a filepath of myApp/config.json might return the following on linux
+//	"./config.json"
 // 	"/home/user/.config/myApp/config.json",
 //	"/etc/xdg/myApp/config.json",
-//	"./config.json"
-// Note that parent folder paths (myApp in this example) are stripped for the last eligble file location
+// Note that parent folder paths (myApp in this example) are stripped for the first eligible file location
 // so the config file will exist in the same directory as the running executable
 func StandardFileLocations(cfgPath string) []string {
-	//get running dir
 	locations := append(userLocations(), systemLocations()...)
 
 	for i := range locations {
@@ -51,12 +50,13 @@ func StandardFileLocations(cfgPath string) []string {
 		}
 	}
 
+	//get running dir
 	runningDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
 		runningDir = "."
 	}
 	runningDir = filepath.Join(runningDir, filepath.Base(cfgPath))
 
-	locations = append(locations, runningDir)
+	locations = append([]string{runningDir}, locations...)
 	return locations
 }
